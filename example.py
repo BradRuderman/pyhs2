979f900fe4aead27874f98f1f0ad52fd8e038de8
Brad Ruderman <bradruderman@gmail.com>
2013-10-26 20:16:47 -0700

    Bumping version

---
+++ example.py
@@ -0,0 +1,12 @@
+import pyhs2
+
+with pyhs2.connect(host='localhost',
+                   port=10000,
+                   authMechanism="PLAIN",
+                   user='root',
+                   password='test',
+                   database='default') as conn:
+    with conn.cursor() as cur:
+        cur.execute("show tables")
+        for i in cur.fetch():
+            print i
